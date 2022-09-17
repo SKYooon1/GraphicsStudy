3a0cd569a591c19fd1ea67fe5d0418d497f04155
SKYooon <ucs0191@naver.com>
2022-09-17 13:04:07 +0900

    워밍업 문제 2,3 파일에서 데이터 읽어 문자열 다루기 - 파일 읽기

--- GraphicsStudy/warmingUp2.cpp
+++ GraphicsStudy/warmingUp2.cpp
@@ -5,30 +5,33 @@
 
 using namespace std;
 
-void help();
+void readFile(ifstream& in, string& str);
+void help()
+{
+	cout << "명령어 설명" << endl;
+	cout << "d : 문장 전체를 뒤집기" << endl;
+	cout << "e : 동일한 간격으로 특정 문자 삽입" << endl;
+	cout << "f : 띄어쓰기를 기준으로 문자 뒤집기" << endl;
+	cout << "g : 원하는 문자를 다른 문자로 바꾸기" << endl;
+	cout << "r : 앞에서부터 읽었을 때와 뒤에서부터 읽었을 때 같은 문자 출력하기" << endl;
+	cout << "h : 명령어 설명" << endl;
+	cout << "q : 프로그램 종료" << endl << endl;
+}
 
 int main()
 {
 	char key{};
 	string str{};
+	ifstream in{};
 
-	cout << "Input the file's name: ";
-	cin >> str;
-
-	ifstream myFile{ str };
-
-	while (!myFile.is_open())
-	{
-		cout << "The file is not exist. Input the file's name: ";
-		cin >> str;
-		myFile.open(str);
-	}
+	readFile(in, str);
 
 	while (true)
 	{
 		help();
-		cout << "Input command";
+		cout << "Input command" << endl;
 		key = _getch();
+		cout << key << endl;
 
 		switch (key)
 		{
@@ -45,14 +48,29 @@ int main()
 	}
 }
 
-void help()
+void readFile(ifstream& in, string& str)
 {
-	cout << "명령어 설명" << endl;
-	cout << "d : 문장 전체를 뒤집기" << endl;
-	cout << "e : 동일한 간격으로 특정 문자 삽입" << endl;
-	cout << "f : 띄어쓰기를 기준으로 문자 뒤집기" << endl;
-	cout << "g : 원하는 문자를 다른 문자로 바꾸기" << endl;
-	cout << "r : 앞에서부터 읽었을 때와 뒤에서부터 읽었을 때 같은 문자 출력하기" << endl;
-	cout << "h : 명령어 설명" << endl;
-	cout << "q : 프로그램 종료" << endl << endl;
+	cout << "Input the file's name: ";
+	cin >> str;
+
+	in.open(str);
+
+	while (!in.is_open())
+	{
+		cout << "The file is not exist. Input the file's name: ";
+		cin >> str;
+		in.open(str);
+	}
+
+	in.seekg(0, std::ios::end);
+
+	const int size = in.tellg();
+
+	in.seekg(0, std::ios::beg);
+
+	str.resize(size);
+
+	in.read(&str[0], size);
+
+	cout << endl << str << endl << endl;
 }
