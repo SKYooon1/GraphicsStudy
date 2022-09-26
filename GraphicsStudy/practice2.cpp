245a89a3abb595f87df8b03f1315fe8c0b86822a
SKYooon <ucs0191@naver.com>
2022-09-26 12:39:52 +0900

    실습 2 - 윈도우 폭과 넓이 조절에 따라 동작 가능하게 변경

--- GraphicsStudy/practice2.cpp
+++ GraphicsStudy/practice2.cpp
@@ -7,23 +7,24 @@
 #define WINDOW_NAME "practice2"
 #define WINDOW_POS_X 100
 #define WINDOW_POS_Y 100
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
 
 GLvoid drawScene(GLvoid);							// 그리기 콜백함수
 GLvoid reshape(int w, int h);						// 다시 그리기 콜백 함수
 GLvoid keyboard(unsigned char key, int x, int y);	// 키보드 콜백 함수
-GLvoid mouse(int button, int state, int x, int y);
+GLvoid mouseClick(int button, int state, int x, int y);	// 마우스 클릭 콜백 함수
 
 void convertCoordinateWinToGl(const int x, const int y, float& ox, float& oy);
+
+static int windowWidth{ 800 };
+static int windowHeight{ 600 };
 
 static float red{ 1 }, green{ 1 }, blue{ 1 };
 static float boxRed{}, boxGreen{}, boxBlue{};
 static float boxWidth{0.25}, boxHeight{0.25};
+
 static std::random_device rd;
 static std::mt19937 gen(rd());
 static std::uniform_real_distribution<float> urd(0, 1);
-bool isTimer{};
 
 void main(int argc, char** argv)
 {
@@ -31,7 +32,7 @@ void main(int argc, char** argv)
 	glutInit(&argc, argv);							// glut 초기화
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);	// 디스플레이 모드 설정
 	glutInitWindowPosition(WINDOW_POS_X, WINDOW_POS_Y);	// 윈도우의 위치 설정
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);	// 윈도우의 크기 지정
+	glutInitWindowSize(windowWidth, windowHeight);	// 윈도우의 크기 지정
 	glutCreateWindow(WINDOW_NAME);			// 윈도우 생성("윈도우 이름")
 
 	// GLEW 초기화
@@ -46,7 +47,7 @@ void main(int argc, char** argv)
 	glutDisplayFunc(drawScene);		// 출력 함수 지정
 	glutReshapeFunc(reshape);		// 다시 그리기 함수 지정
 	glutKeyboardFunc(keyboard);
-	glutMouseFunc(mouse);
+	glutMouseFunc(mouseClick);
 
 	glutMainLoop();					// 이벤트 처리 시작
 }
@@ -64,6 +65,8 @@ GLvoid drawScene(GLvoid)
 
 GLvoid reshape(int w, int h)
 {
+	windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+	windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
 	glViewport(0, 0, w, h);
 }
 
@@ -80,10 +83,11 @@ GLvoid keyboard(unsigned char key, int x, int y)
 	glutPostRedisplay();
 }
 
-GLvoid mouse(int button, int state, int x, int y)
+GLvoid mouseClick(int button, int state, int x, int y)
 {
 	float ox{}, oy{};
 	convertCoordinateWinToGl(x, y, ox, oy);
+
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
 		if (ox >= -boxWidth && ox <= boxWidth && oy >= -boxHeight && oy <= boxHeight)
@@ -117,8 +121,8 @@ GLvoid mouse(int button, int state, int x, int y)
 
 void convertCoordinateWinToGl(const int x, const int y, float& ox, float& oy)
 {
-	constexpr float w{ WINDOW_WIDTH };
-	constexpr float h{ WINDOW_HEIGHT };
+	const float w{ static_cast<float>(windowWidth) };
+	const float h{ static_cast<float>(windowHeight) };
 	ox = { (static_cast<float>(x) - (w / 2.0f)) * (1.0f / (w / 2.0f)) };
 	oy = { -(static_cast<float>(y) - (h / 2.0f)) * (1.0f / (h / 2.0f)) };
 }
